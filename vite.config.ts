c0db766f46d5c0d6a641a79a904de2efdc2312af
hogoshi <hogoshi@yandex.ru>
2025-05-11 17:26:08 +0700

    ci/cd fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hogoshi.dev/'
+  base: '/hogoshi.dev/',
+  server: {
+    port: 3000,
+    open: true
+  }
 })
